221b402b65000993557d13f740064e7e739a28da
dilan <dilantunc1124@gmail.com>
2025-06-21 17:33:28 +0300

    Düzenleme yapıldı

--- src/pages/admin/HaberlerPage.jsx
+++ src/pages/admin/HaberlerPage.jsx
@@ -6,10 +6,10 @@ import {
 import axios from "axios";
 import { db } from "../../../firebase-config";
 import {
-  collection, getDocs, doc, query, orderBy
+  collection, getDocs, query, orderBy
 } from "firebase/firestore";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE_URL = import.meta.env.REACT_APP_API_URL;
 
 const HaberlerPage = () => {
   const [news, setNews] = useState([]);
